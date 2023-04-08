dec8f6613a65cc313e1a581dcf9a6330d7be5f4f
Ankit <Ankit@Ankit>
2023-04-08 23:22:57 +0530

    How am i

--- sdacprac/src/sdacprac/Ankit2.java
+++ sdacprac/src/sdacprac/Ankit2.java
@@ -1,7 +1,7 @@
 package sdacprac;
 
 public class Ankit2 {
-	public static void main(String[] args) {
-		System.out.println("How Are you Ankit Today");
-	}
+public static void main(String[] args) {
+	System.out.println("Kaisa Hoon main ");
+}
 }
